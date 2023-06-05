d9e41f52f2953a6140b806e1f1e4d0649981ceb4
DekelA <dekel16791@gmail.com>
2023-06-05 20:24:26 +0300

    Update

---
+++ WeatherApp/Controllers/CityWeatherController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherApp.Models;
+
+namespace WeatherApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("home")]
+        [Route("/")]
+        public IActionResult Home()
+        {
+            ViewData["appTitle"] = "WeatherApp";
+            var citiesWeather = CityWeather.GetCityWeathers();
+            return View(citiesWeather);
+        }
+
+        [Route("/details/{cityCode}")]
+        public IActionResult Details(string cityCode)
+        {
+            var matchedCity = CityWeather.GetCityByUniqueCode(cityCode);
+            return View(matchedCity);
+        }
+    }
+}
---
+++ WeatherApp/Models/CityWeather.cs
@@ -0,0 +1,38 @@
+﻿namespace WeatherApp.Models
+{
+    public class CityWeather
+    {
+        #region Properties
+
+        public string? CityUniqueCode { get; set; }
+        public string? CityName { get; set; }
+        public DateTime? DateAndTime { get; set; }
+        public int? TempCelsius { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public static CityWeather? GetCityByUniqueCode(string uniqueCode)
+        {
+            var cities = GetCityWeathers();
+            var matchedCityWeather = cities.Where(temp => temp.CityUniqueCode == uniqueCode).FirstOrDefault();
+
+            return matchedCityWeather;
+        }
+
+        public static List<CityWeather> GetCityWeathers()
+        {
+            List<CityWeather> cityWeathers = new()
+            {
+                new CityWeather() {CityUniqueCode = "LDN", CityName = "London", DateAndTime = Convert.ToDateTime("2030-01-01 8:00"),  TempCelsius = 6},
+                new CityWeather() {CityUniqueCode = "TLV", CityName = "Tel-Aviv", DateAndTime = Convert.ToDateTime("2030-01-01 3:00"),  TempCelsius = 30},
+                new CityWeather() {CityUniqueCode = "NYC", CityName = "New-York-City", DateAndTime = Convert.ToDateTime("2030-01-01 9:00"),  TempCelsius = 8},
+            };
+
+            return cityWeathers;
+        }
+
+        #endregion
+    }
+}
